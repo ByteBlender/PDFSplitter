47d5b149f717ea009a6481f289a94db4896f5aec
Gayan Wijesinghe <gayan@mailingsolutions.net.au>
2021-01-06 14:15:21 +0800

    paramterized file names and folders

--- PDFSplitter/Program.cs
+++ PDFSplitter/Program.cs
@@ -14,9 +14,15 @@ namespace PDFSplitter
 {
     class Program
     {
-
+        static string jobDir = @"S:\DATABASES\AAA MISCELLANEOUS\MAIL MAKERS\JOB 207472 ECU RESULTS MAILING";
+        static string sourcePDF = "ecur1070.2139644.pdf";
+        static string dataFile = "JOB207472DR - Course Completed Semester 2 2020.txt";
         static void Main(string[] args)
         {
+            // MoveFiles();
+            VerifyOutput();
+
+
             Console.WriteLine("1.ExtractIDs\n2.PdfRename\n3.MatchIDs\n4.VerifyFileNames");
             string sw = Console.ReadLine();
 
@@ -79,8 +85,8 @@ namespace PDFSplitter
 
         public static void ExtractIDs()
         {
-            PdfReader reader = new PdfReader(@"S:\DATABASES\AAA MISCELLANEOUS\MAIL MAKERS\JOB 207472 ECU RESULTS MAILING\PDF Extraction\Sem 2 2020 Main BoE Transcripts.pdf");
-            FileStream fs = new FileStream(@"S:\DATABASES\AAA MISCELLANEOUS\MAIL MAKERS\JOB 207472 ECU RESULTS MAILING\PDF Extraction\extracted_test.txt", FileMode.Create);
+            PdfReader reader = new PdfReader($@"{jobDir}\PDF Extraction\temp\{sourcePDF}");
+            FileStream fs = new FileStream($@"{jobDir}\PDF Extraction\temp\extractedIDs.txt", FileMode.Create);
             StreamWriter sw = new StreamWriter(fs);
             PdfReaderContentParser parser = new PdfReaderContentParser(reader);
             ITextExtractionStrategy strategy;
@@ -88,8 +94,9 @@ namespace PDFSplitter
             string previousVal = "";
             string currentVal = "";
             int count = 0;
+            string pages = "";
 
-            sw.WriteLine("Index\tID\tPageNumber\tFileName");
+            sw.WriteLine("Index\tID\tPageCounter\tPageNumber\tFileName");
 
             for (int i = 1; i <= reader.NumberOfPages; i++)
             {
@@ -102,13 +109,19 @@ namespace PDFSplitter
                     strategy = new FilteredTextRenderListener(new LocationTextExtractionStrategy(), filter);
                     currentVal = PdfTextExtractor.GetTextFromPage(reader, i, strategy);
 
-                    if(previousVal != currentVal)
+                    Rectangle area2 = new Rectangle(465, 565, 555, 635);
+                    RenderFilter filter2 = new RegionTextRenderFilter(area2);
+                    strategy = new FilteredTextRenderListener(new LocationTextExtractionStrategy(), filter2);
+                    pages = PdfTextExtractor.GetTextFromPage(reader, i, strategy);
+
+
+                    if (previousVal != currentVal)
                     {
                         count = 0;
                     }
                     count++;
                     previousVal = currentVal;
-                    sw.WriteLine($"{i}\t{currentVal}\t{count}\t{currentVal}-{count}");
+                    sw.WriteLine($"{i}\t{currentVal}\t{pages.Split('\n')[0]}\t{count}\t{currentVal}-{count}");
                 }
                 catch (Exception)
                 {
@@ -119,12 +132,60 @@ namespace PDFSplitter
             sw.Close();
         }
 
+        public static void VerifyOutput()
+        {
+            PdfReader reader = new PdfReader($@"{jobDir}\207472 ECU Transcripts.pdf");
+            FileStream fs = new FileStream($@"{jobDir}\PDF Extraction\temp\extractedIDs_test.txt", FileMode.Create);
+            StreamWriter sw = new StreamWriter(fs);
+            PdfReaderContentParser parser = new PdfReaderContentParser(reader);
+            ITextExtractionStrategy strategy;
+            TextMarginFinder finder;
+            string previousVal = "";
+            string currentVal = "";
+            int count = 0;
+            string pages = "";
+
+            sw.WriteLine("Index\tID\tPageCounter\tPageNumber\tFileName");
+
+            for (int i = 1; i <= reader.NumberOfPages; i++)
+            {
+                try
+                {
+                    // finder = parser.ProcessContent(i, new TextMarginFinder());
+                    //Rectangle area = new Rectangle(finder.GetLlx(), finder.GetLly(), finder.GetWidth() / 2, finder.GetHeight() / 2);
+                    Rectangle area = new Rectangle(61, 600, 265, 680);
+                    RenderFilter filter = new RegionTextRenderFilter(area);
+                    strategy = new FilteredTextRenderListener(new LocationTextExtractionStrategy(), filter);
+                    currentVal = PdfTextExtractor.GetTextFromPage(reader, i, strategy);
+
+                    //Rectangle area2 = new Rectangle(465, 565, 555, 635);
+                    //RenderFilter filter2 = new RegionTextRenderFilter(area2);
+                    //strategy = new FilteredTextRenderListener(new LocationTextExtractionStrategy(), filter2);
+                    //pages = PdfTextExtractor.GetTextFromPage(reader, i, strategy);
+
+
+                    if (previousVal != currentVal)
+                    {
+                        count = 0;
+                    }
+                    count += 2;
+                    previousVal = currentVal;
+                    sw.WriteLine($"{i}\t{currentVal}\t{pages.Split('\n')[0]}\t{count}\t{currentVal}-{count}");
+                }
+                catch (Exception)
+                {
+                    sw.WriteLine($"{i}\tfailed");
+                }
+            }
+            sw.Flush();
+            sw.Close();
+        }
         public static void MatchIDs()
         {
-            DataTable source = TextFileRW.readTextFileToTable(@"S:\DATABASES\AAA MISCELLANEOUS\MAIL MAKERS\JOB 207024 ECU RESULTS MAILING\JOB207024DR - 201 Course Complete Address details by Date.txt", "\t");
+            DataTable source = TextFileRW.readTextFileToTable($@"{jobDir}\{dataFile}", "\t");
             source.Columns.Add("PageCount");
             source.Columns.Add("Index");
-            DataTable sample = TextFileRW.readTextFileToTable(@"S:\DATABASES\AAA MISCELLANEOUS\MAIL MAKERS\JOB 207024 ECU RESULTS MAILING\PDF Extraction\Page counts.txt", "\t");
+            DataTable sample = TextFileRW.readTextFileToTable($@"{jobDir}\PDF Extraction\PageCounts.txt", "\t");
             sample.Columns.Add("Matched");
 
             foreach (DataRow sourceRow in source.Rows)
@@ -141,8 +202,8 @@ namespace PDFSplitter
 
             }
 
-            TextFileRW.writeTableToTxtFile(source, @"S:\DATABASES\AAA MISCELLANEOUS\MAIL MAKERS\JOB 207024 ECU RESULTS MAILING\JOB207024DR - 201 Course Complete Address_ID_merged.txt", "\t");
-            TextFileRW.writeTableToTxtFile(sample, @"S:\DATABASES\AAA MISCELLANEOUS\MAIL MAKERS\JOB 207024 ECU RESULTS MAILING\Transcripts List.txt", "\t");
+            TextFileRW.writeTableToTxtFile(source, $@"{jobDir}\{dataFile.Replace(".txt","")}_merged.txt", "\t");
+            TextFileRW.writeTableToTxtFile(sample, $@"{jobDir}\Transcripts List.txt", "\t");
 
         }
 
@@ -150,10 +211,10 @@ namespace PDFSplitter
         {
 
 
-            string dir = @"S:\DATABASES\AAA MISCELLANEOUS\MAIL MAKERS\JOB 207024 ECU RESULTS MAILING\PDF Extraction\Transcripts\";
+            string dir = $@"{jobDir}\PDF Extraction\Transcripts\";
 
             string[] files = Directory.GetFiles(dir);
-            DataTable dt = TextFileRW.readTextFileToTable(@"S:\DATABASES\AAA MISCELLANEOUS\MAIL MAKERS\JOB 207024 ECU RESULTS MAILING\PDF Extraction\extracted.txt", "\t");
+            DataTable dt = TextFileRW.readTextFileToTable($@"{jobDir}\PDF Extraction\extractedIDs.txt", "\t");
             Dictionary<int, string> names = new Dictionary<int, string>();
             foreach (DataRow r in dt.Rows)
             {
@@ -164,24 +225,62 @@ namespace PDFSplitter
             {
 
                 string f = System.IO.Path.GetFileName(file);
-                int index = int.Parse(f.Replace("201 Course Completed Transcripts Version 2_CLEANED_Part", "").Replace(".pdf", ""));
+                int index = int.Parse(f.Replace($"{sourcePDF.Replace(".pdf","")}_Part", "").Replace(".pdf", ""));
 
                 Directory.Move(file, $@"{dir}\{names[index]}.pdf");
             }
 
         }
 
+        public static void MoveFiles()
+        {
+
+
+            string dir = $@"{jobDir}\PDF Extraction\Transcripts\";
+
+            string[] files = Directory.GetFiles(dir);
+            DataTable dt = TextFileRW.readTextFileToTable($@"{jobDir}\PDF Extraction\Bad Files.txt", "\t");
+            Dictionary<int, string> names = new Dictionary<int, string>();
+           
+
+            foreach (var file in files)
+            {
+
+                string f = System.IO.Path.GetFileName(file);
+
+
+                foreach (DataRow r in dt.Rows)
+                {
+                    if(file== r[0].ToString())
+                    {
+                        Directory.Move(file, $@"{dir}\bad files\{f}");
+                    }
+                }
+        
+            }
+
+        }
+
         public static string ReadID(string fileName)
         {
-            PdfReader reader = new PdfReader(fileName);
-            PdfReaderContentParser parser = new PdfReaderContentParser(reader);
-            ITextExtractionStrategy strategy;
-            //TextMarginFinder finder;
-            //finder = parser.ProcessContent(1, new TextMarginFinder());
-            Rectangle area = new Rectangle(414, 660, 522, 689);
-            RenderFilter filter = new RegionTextRenderFilter(area);
-            strategy = new FilteredTextRenderListener(new LocationTextExtractionStrategy(), filter);
-            return PdfTextExtractor.GetTextFromPage(reader, 1, strategy);    
+            try
+            {
+                PdfReader reader = new PdfReader(fileName);
+                PdfReaderContentParser parser = new PdfReaderContentParser(reader);
+                ITextExtractionStrategy strategy;
+                //TextMarginFinder finder;
+                //finder = parser.ProcessContent(1, new TextMarginFinder());
+                Rectangle area = new Rectangle(414, 660, 522, 689);
+                RenderFilter filter = new RegionTextRenderFilter(area);
+                strategy = new FilteredTextRenderListener(new LocationTextExtractionStrategy(), filter);
+                return PdfTextExtractor.GetTextFromPage(reader, 1, strategy);
+            }
+            catch (Exception)
+            {
+
+                return "unreadable";
+            }
+          
         }
 
         public static void VerifyFileNames()
@@ -191,7 +290,7 @@ namespace PDFSplitter
             dt.Columns.Add("ExtractedID");
             dt.Columns.Add("Match");
 
-            string dir = @"S:\DATABASES\AAA MISCELLANEOUS\MAIL MAKERS\JOB 207024 ECU RESULTS MAILING\PDF Extraction\Transcripts\";
+            string dir = $@"{jobDir}\PDF Extraction\Transcripts\Fixed\New folder";
             string[] files = Directory.GetFiles(dir);
             string id = "";
             foreach (var file in files)
@@ -204,7 +303,7 @@ namespace PDFSplitter
                 dt.Rows.Add(r);
             }
 
-            TextFileRW.writeTableToTxtFile(dt, @"S:\DATABASES\AAA MISCELLANEOUS\MAIL MAKERS\JOB 207024 ECU RESULTS MAILING\PDF Extraction\FileNameVerification.txt", "\t");
+            TextFileRW.writeTableToTxtFile(dt, $@"{jobDir}\PDF Extraction\FileNameVerification.txt", "\t");
         }
 
     }
